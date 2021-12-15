d0e425586960aad893337f88cb8eb6890b4f8411
AliceMenzie <alice.menzie@gmail.com>
2021-12-15 11:41:57 +1100

    Add Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div>
+            00 Home 01 Destination 02 Crew 03 Technology
+            
+        </div>
+    )
+}
+
+export default NavBar
