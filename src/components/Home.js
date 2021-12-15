e366f58db422d87ee7ea651f1332138ed3fe4a01
AliceMenzie <alice.menzie@gmail.com>
2021-12-15 16:27:06 +1100

    Add styling to home page

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home" >
+      <div className="grid-container grid-container--home ">
+        <div>
+        <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to
+      <span className="fs-900 ff-serif text-white d-block"> Space</span></h1>
+          <p className="self-center">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div>
+          <Link
+            className="large-button uppercase ff-serif text-dark bg-white fs-600"
+            to="/destination"
+          >
+            Explore
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
