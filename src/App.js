86dfdd858778c739accfef2e7117625ef6238ae4
AliceMenzie <alice.menzie@gmail.com>
2021-12-16 15:16:57 +1100

    add assets to mobile navigation

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Router>
-        {/* <NavBar /> */}
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/destination" element={<Destination />}></Route>
